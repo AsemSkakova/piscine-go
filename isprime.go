55382593a73471c9963a30c9dcaabc3803f67155
AsemSkakova <asemaskakova@gmail.com>
2019-10-21 21:37:52 +0600

    4: isprime

--- isprime.go
+++ isprime.go
@@ -2,14 +2,15 @@ package piscine
 
 func IsPrime(nb int) bool {
 
-	for i := 2; i < nb; i++ {
+	if nb < 2 {
+		return false
+	}
+
+	for i := nb - 1; i > 1; i-- {
 		if nb%i == 0 {
 			return false
 		}
 	}
 
-	//if nb%nb == 0 && nb%1 == 0 {
-	//return true
-	//}
 	return true
 }
