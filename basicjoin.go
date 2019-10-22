e8f0932029d91accc3e577928f839ea1dfcacd40
AsemSkakova <asemaskakova@gmail.com>
2019-10-22 22:21:26 +0600

    jkl

--- basicjoin.go
+++ basicjoin.go
@@ -1,8 +1,12 @@
 package piscine
 
-func BasicJoin(strs []string) string {
+func Join(strs []string, sep string) string {
 	var str string
-	for _, words := range strs {
+	for index, words := range strs {
+		if index != 0 {
+			str += sep
+
+		}
 		str += words
 	}
 	return str
