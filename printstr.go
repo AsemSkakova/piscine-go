7c2a45b8cc5cfd1ddf3c591c0ccc587e5432eba5
AsemSkakova <asemaskakova@gmail.com>
2019-10-23 14:15:54 +0600

    all

---
+++ printstr.go
@@ -0,0 +1,15 @@
+package piscine
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func PrintStr(str string) {
+
+	//strToRune := []rune(str)
+
+	for _, word := range []rune(str) {
+		z01.PrintRune(word)
+	}
+	z01.PrintRune('\n')
+}
