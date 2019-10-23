ba1b92a3068d0189536f462f4cba3d8c1e400552
AsemSkakova <asemaskakova@gmail.com>
2019-10-23 19:59:13 +0600

    test

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -3,9 +3,14 @@ package piscine
 func IterativeFactorial(nb int) int {
 
 	result := 1
+	if nb > 0 {
+		for i := 1; i <= nb; i++ {
+			result = result * i
+		}
 
-	for i := 1; i <= nb; i++ {
-		result = result * i
+		if nb < 0 {
+			return 0
+		}
 	}
 	return result
 }
