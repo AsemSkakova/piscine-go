3ed175988faf19f1f85e2e0249742800455f2799
AsemSkakova <asemaskakova@gmail.com>
2019-10-24 15:09:52 +0600

    5: alphacount

--- alphacount.go
+++ alphacount.go
@@ -2,15 +2,14 @@ package piscine
 
 func AlphaCount(str string) int {
 
-	strToByte := []byte(str)
-
+	a := []rune(str)
 	counter := 0
 
-	for _, letter := range strToByte {
-
+	for _, letter := range a {
 		if (letter >= 65 && letter <= 90) || (letter >= 97 && letter <= 122) {
 			counter++
 		}
 	}
 	return counter
+
 }
