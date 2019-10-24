03f3bdcd40b15cda9fecf1f6dd30d5463886f7bc
AsemSkakova <asemaskakova@gmail.com>
2019-10-24 16:54:26 +0600

    5: toupper

--- toupper.go
+++ toupper.go
@@ -1,11 +1,14 @@
 package piscine
 
 func ToUpper(s string) string {
-	sAsByte := []byte(s)
-	for index, letter := range sAsByte {
-		if letter <= 122 && letter >= 97 {
-			sAsByte[index] = letter - 32
+
+	a := []rune(s)
+
+	for index, letter := range s {
+		if letter >= 97 && letter <= 122 {
+			a[index] = letter - 32
 		}
 	}
-	return string(sAsByte)
+
+	return string(a)
 }
