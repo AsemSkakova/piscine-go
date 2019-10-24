fed60b643bd7627832e05e8d822b03da6ba814a6
AsemSkakova <asemaskakova@gmail.com>
2019-10-24 16:09:39 +0600

    5: nrune

--- nrune.go
+++ nrune.go
@@ -1,12 +1,9 @@
 package piscine
 
 func NRune(s string, n int) rune {
-	strAsByte := []rune(s)
-	var nrune rune
-	for index, letter := range strAsByte {
-		if index == n-1 {
-			nrune = letter
-		}
-	}
-	return nrune
+
+	a := []rune(s)
+
+	return a[n-1]
+
 }
