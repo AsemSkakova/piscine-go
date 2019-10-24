b9850dc53c714d2653d0c0c4af1d5ef63c16fbfe
AsemSkakova <asemaskakova@gmail.com>
2019-10-24 17:34:57 +0600

    5: capitalize

--- capitalize.go
+++ capitalize.go
@@ -1,27 +1,27 @@
 package piscine
 
 func Capitalize(s string) string {
-	sAsRune := []rune(s)
-	for index, letter := range sAsRune {
-		if checkAlphNum(letter) {
-			if index == 0 || checkAlphNum(sAsRune[index-1]) == false {
-				if letter >= 'a' && letter <= 'z' {
-					sAsRune[index] = letter - 32
+	a := []rune(s)
+
+	for index, letter := range a {
+		if checkAlpNum(letter) {
+			if index == 0 || checkAlpNum(a[index-1]) == false {
+				if letter >= 97 && letter <= 122 {
+					a[index] = letter - 32
 				}
 			} else {
 				if letter >= 'A' && letter <= 'Z' {
-					sAsRune[index] = letter + 32
+					a[index] = letter + 32
 				}
 			}
 		}
 	}
-	return string(sAsRune)
+	return string(a)
+
 }
 
-func checkAlphNum(r rune) bool {
-	if r >= 'A' && r <= 'Z' ||
-		r >= 'a' && r <= 'z' ||
-		r >= '0' && r <= '9' {
+func checkAlpNum(r rune) bool {
+	if r >= 'A' && r <= 'Z' || r >= 'a' && r <= 'z' || r >= 0 && r <= 9 {
 		return true
 	}
 	return false
