0f3e8536d1572b67e543a511e4fd945e1eec85c9
AsemSkakova <asemaskakova@gmail.com>
2019-10-24 19:47:01 +0600

    5: isprintable

--- isprintable.go
+++ isprintable.go
@@ -4,7 +4,7 @@ func IsPrintable(str string) bool {
 
 	a := []rune(str)
 	for _, letter := range a {
-		if checkAlp(letter) == false {
+		if checkPrint(letter) {
 			return false
 		}
 	}
@@ -12,10 +12,8 @@ func IsPrintable(str string) bool {
 
 }
 
-func checkAlp(r rune) bool {
-	if r >= 'A' && r <= 'Z' ||
-		r >= 'a' && r <= 'z' ||
-		r >= '0' && r <= '9' {
+func checkPrint(r rune) bool {
+	if r >= 0 && r <= 31 {
 		return true
 	}
 	return false
